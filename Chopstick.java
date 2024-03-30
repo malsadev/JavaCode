c82f6152223b1231011c7f14ccfda56019c0c459
malsa080 <malsa080@uottawa.ca>
2024-03-30 17:19:14 -0400

    Implemented wait/notify on chopstick class

--- Chopstick.java
+++ Chopstick.java
@@ -1,22 +1,38 @@
 public class Chopstick {
 	private int ID;
-// hint: use a local variable to indicate whether the chopstick is free 
-//                        (lying on the table), e.g.  private boolean free;
+	// hint: use a local variable to indicate whether the chopstick is free
+	// (lying on the table), e.g. private boolean free;
+
+	private boolean free = true;
 
 	Chopstick(int ID) {
-		  this.ID = ID;
-	
+		this.ID = ID;
+
 	}
-	
-	synchronized void take() {
-	
+
+	synchronized void take() throws InterruptedException {
+
+		if (!free) {
+			wait();
+		}
+
+		free = false;
+		notify();
 	}
-	
-	synchronized void release() {
-	
+
+	synchronized void release() throws InterruptedException {
+
+		if (free) {
+			wait();
+		}
+
+		free = true;
+
+		notify();
+
 	}
-	    
+
 	public int getID() {
-	    return(ID);
+		return (ID);
 	}
 }
